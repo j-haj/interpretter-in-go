9d8f8d0850baac7041194e19031c68df81865374
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-08-31 06:49:13 -0500

    Add additional math operators to lexer

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -14,8 +14,15 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
